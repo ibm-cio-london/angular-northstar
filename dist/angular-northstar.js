f7c2e7a3357b4d8f3a3582fadfa8ed469ba385e1
Tom Ratcliffe <tomratcliffe@uk.ibm.com>
2016-09-30 16:03:12 +0100

    Initial Alpha Release

--- dist/angular-northstar.js
+++ dist/angular-northstar.js
@@ -11,7 +11,6 @@
     angular
         .module( 'angular-northstar', [
             'angular-northstar.checkbox',
-            'angular-northstar.menunav',
             'angular-northstar.select',
             'angular-northstar.setsameheight',
             'angular-northstar.showhide',
@@ -47,6 +46,21 @@
     'use strict';
 
     angular
+        .module( 'angular-northstar.setsameheight', [] );
+} )();
+
+/*
+ * Licensed Materials - Property of IBM
+ *
+ * (C) Copyright IBM Corp. 2016 All Rights Reserved
+ *
+ * US Government Users Restricted Rights - Use, duplication, or
+ * disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
+ **/
+( function () {
+    'use strict';
+
+    angular
         .module( 'angular-northstar.select', [] );
 } )();
 
@@ -63,21 +77,6 @@
 
     angular
         .module( 'angular-northstar.showhide', [] );
-} )();
-
-/*
- * Licensed Materials - Property of IBM
- *
- * (C) Copyright IBM Corp. 2016 All Rights Reserved
- *
- * US Government Users Restricted Rights - Use, duplication, or
- * disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
- **/
-( function () {
-    'use strict';
-
-    angular
-        .module( 'angular-northstar.setsameheight', [] );
 } )();
 
 /*
@@ -189,6 +188,57 @@
                 $timeout( function () {
                     IBMCore.common.widget.checkboxradio.init( element );
                 } );
+            }
+        };
+    }
+} )();
+
+/*
+ * Licensed Materials - Property of IBM
+ *
+ * (C) Copyright IBM Corp. 2016 All Rights Reserved
+ *
+ * US Government Users Restricted Rights - Use, duplication, or
+ * disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
+ **/
+
+/**
+ * @ngdoc directive
+ * @name northstarSetsameheight.directive:northstarSetsameheight
+ * @restrict A
+ * @description
+ * # Why do we need it?
+ * By default, if you include a set same height widget on a v18 page which is dynamically injected
+ * into the page, it is included too late to be initialised by the `www.js` file.
+ *
+ * If we don't initialise the set same height element, the correct styles won't be applied and v18
+ * design standards won't be met
+ *
+ * # What does this directive do?
+ * todo
+ *
+ * @author Matthew Parish <matthewparish@uk.ibm.com>
+ */
+( function () {
+    'use strict';
+    angular
+        .module( 'angular-northstar.setsameheight' )
+        .directive( 'northstarSetsameheight', ['$timeout', northstarSetsameheight] );
+
+    function northstarSetsameheight ( $timeout ) {
+        return {
+            restrict: 'A',
+            link: function ( $scope, element ) {
+                function init () {
+                    // Wait until next angular cycle before initialising
+                    $timeout( function () {
+                        jQuery(element).setsameheight();
+                    } );
+                }
+
+                $scope.$on( 'rebuildSetSameHeight', init );
+
+                init();
             }
         };
     }
@@ -323,57 +373,6 @@
                 $timeout( function () {
                     jQuery(element).showhide();
                 } );
-            }
-        };
-    }
-} )();
-
-/*
- * Licensed Materials - Property of IBM
- *
- * (C) Copyright IBM Corp. 2016 All Rights Reserved
- *
- * US Government Users Restricted Rights - Use, duplication, or
- * disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
- **/
-
-/**
- * @ngdoc directive
- * @name northstarSetsameheight.directive:northstarSetsameheight
- * @restrict A
- * @description
- * # Why do we need it?
- * By default, if you include a set same height widget on a v18 page which is dynamically injected
- * into the page, it is included too late to be initialised by the `www.js` file.
- *
- * If we don't initialise the set same height element, the correct styles won't be applied and v18
- * design standards won't be met
- *
- * # What does this directive do?
- * todo
- *
- * @author Matthew Parish <matthewparish@uk.ibm.com>
- */
-( function () {
-    'use strict';
-    angular
-        .module( 'angular-northstar.setsameheight' )
-        .directive( 'northstarSetsameheight', ['$timeout', northstarSetsameheight] );
-
-    function northstarSetsameheight ( $timeout ) {
-        return {
-            restrict: 'A',
-            link: function ( $scope, element ) {
-                function init () {
-                    // Wait until next angular cycle before initialising
-                    $timeout( function () {
-                        jQuery(element).setsameheight();
-                    } );
-                }
-
-                $scope.$on( 'rebuildSetSameHeight', init );
-
-                init();
             }
         };
     }
