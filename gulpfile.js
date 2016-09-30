34910a593a27c6a777a7c3cc05dc6b4a64eb4bae
Tom Ratcliffe <tomratcliffe@uk.ibm.com>
2016-09-30 14:22:53 +0100

    Updated build process and dist folder files

--- gulpfile.js
+++ gulpfile.js
@@ -11,12 +11,16 @@
     var lintTask = require( './tasks/lint' )( gulp, plugins, pkg );
     var cleanTask = require( './tasks/clean' )( gulp, plugins, pkg );
     var testTask = require( './tasks/test' )();
+    var copyTask = require( './tasks/copy' )( gulp, plugins, pkg );
 
     gulp.task( 'clean', cleanTask );
-    gulp.task( 'scripts', ['clean']);
+    gulp.task( 'copy', copyTask );
+    gulp.task( 'scripts', ['clean'], scriptsTask);
     gulp.task( 'test', testTask );
     gulp.task( 'lint', lintTask );
 
-    gulp.task( 'build', ['scripts'] );
+    gulp.task( 'build', ['scripts'], copyTask );
+
+    gulp.task( 'default', ['build'] );
 
 } )();
---
+++ tasks/copy.js
@@ -0,0 +1,15 @@
+( function () {
+    'use strict';
+
+
+    module.exports = function eslint ( gulp, gulpPlugins, pkg ) {
+        var dirs = pkg.configParams.dirs;
+
+        var buildDir = dirs.build;
+
+        return function () {
+            return gulp.src( [ './package.json', './README.md', './LICENSE.txt' ] )
+            .pipe( gulp.dest( buildDir ) );
+        };
+    };
+} )();
